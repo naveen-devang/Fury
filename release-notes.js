180c044b14da40ecd0aafa557cb8b888fbbda538
naveen-devang <naveendevang0@gmail.com>
2025-01-22 03:04:42 +0530

    bug fixes & sorting files

---
+++ release-notes.js
@@ -0,0 +1,11 @@
+const RELEASE_NOTES = {
+    "0.0.38": [
+      "Your new features/fixes here"
+    ],
+    "0.0.17": [
+      "Added Hardware Acceleration Support",
+      "Embedded Subtitles now work"
+    ]
+  };
+
+module.exports = RELEASE_NOTES;
